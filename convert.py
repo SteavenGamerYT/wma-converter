1c600b475839262514f34ed76fa61da3fc065701
Kyle Freidhof <freidhofkyle@gmail.com>
2023-11-23 21:00:40 +0000

    Made improvements to the Program/Script and changes.

--- convert.py
+++ convert.py
@@ -1,15 +1,46 @@
 #!/usr/bin/env python3
+"""
+Author: kjfreidhof
 
+Created: November, 3 2023
+
+Program: convert.py
+
+About: Takes a .wma audio file and converts it into a .mp3. It uses ffmpeg to convert the wma audio file into mp3. 
+It does all in one. Given the correct input and output path. This script was designed to work on linux but could in theory workon other opreating systems.
+
+"""
+
+# Importing Python modules 
 import os
-import glob
-from pydub import AudioSegment
+import subprocess
 
-music_dir = '/home/kjfreidhof/Music_mp3/AC-DC'
-extension_list = ('*.wma')
+# A function for converting wma into mp3 give the input path and output path
+def convert_wma_to_mp3(input_file, output_file):
+    
+# The ffmpeg command 
+    ffmpeg_cmd = [
+        "ffmpeg",
+        "-i", input_file,
+        output_file
+    ]
 
-os.chdir(music_dir)
-for extension in extension_list:
-    for music in glob.glob(extension):
-        mp3_filename = os.path.splitext(os.path.basename(music))[0] + '.mp3'
-        AudioSegment.from_file(music).export(mp3_filename, format='mp3')
+# going to try to run the command to convert the file 
+    try:
+        subprocess.run(ffmpeg_cmd, check=True)
+        print("Converted Successfully")
 
+# otherwise if not then error 
+    except subprocess.CalledProcessError as e:
+        print("Not converted Successfully")
+
+# Giving The input and output directory. 
+input_dir = input("Enter the input directory path: ")
+output_dir = input("Enter the output directory path: ")
+
+# Then convert the file or files 
+for filename in os.listdir(input_dir):
+    if filename.endswith(".wma"):
+        input_file = os.path.join(input_dir, filename)
+        output_file = os.path.join(output_dir, filename.rsplit(".", 1)[0] + ".mp3")
+        convert_wma_to_mp3(input_file, output_file)
