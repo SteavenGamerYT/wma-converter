d76123b9064a02d8cf149c560e0abaf554ef1f76
SteavenGamerYT <62666559+SteavenGamerYT@users.noreply.github.com>
2023-12-11 22:40:35 +0200

    chatgpt fy

--- convert.py
+++ convert.py
@@ -2,13 +2,12 @@
 """
 Author: kjfreidhof
 
-Created: November, 3 2023
+Created: November 3, 2023
 
 Program: convert.py
 
 About: Takes a .wma audio file and converts it into a .mp3. It uses ffmpeg to convert the wma audio file into mp3. 
-It does all in one. Given the correct input and output path. This script was designed to work on linux but could in theory work
- on other opreating systems.
+It does all in one. Given the correct input and output path. This script was designed to work on Linux but could, in theory, work on other operating systems.
 
 """
 
@@ -16,26 +15,26 @@ It does all in one. Given the correct input and output path. This script was des
 import os
 import subprocess
 
-# A function for converting wma into mp3 give the input path and output path
+# A function for converting wma into mp3 given the input path and output path
 def convert_wma_to_mp3(input_file, output_file):
     
-# The ffmpeg command 
+    # The ffmpeg command 
     ffmpeg_cmd = [
         "ffmpeg",
         "-i", input_file,
         output_file
     ]
 
-# going to try to run the command to convert the file 
+    # Going to try to run the command to convert the file 
     try:
         subprocess.run(ffmpeg_cmd, check=True)
         print("Converted Successfully")
 
-# otherwise if not then error 
+    # Otherwise, if not, then error 
     except subprocess.CalledProcessError as e:
         print("Not converted Successfully")
 
-# Giving The input and output directory. 
+# Giving the input and output directory. 
 input_dir = input("Enter the input directory path: ")
 output_dir = input("Enter the output directory path: ")
 
